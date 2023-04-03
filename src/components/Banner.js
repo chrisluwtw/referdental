03ed05bee177cbfc2e881fa9f5f28f60f5d277de
tom <cowboymind@gmail.com>
2023-04-03 16:43:00 -0700

    test 6

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Banner() {
   return (
     <div className="banner">
-      <h1>Referdent (Company Name Home Page test 5 acl)</h1>
+      <h1>Referdent (Company Name Home Page test 6 acl)</h1>
       <div className="banner-content">
         <div className="banner-item">
           <h2>Users, people that will have access to Referdental </h2>
